182724ec93be592be350acfc2edcec925bb7d20a
ymc0182 <501414437@qq.com>
2025-10-21 19:04:17 +0800

    update

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,8 +4,20 @@ import { defineCollection, z } from "astro:content";
 const page = defineCollection({
     loader: glob({ pattern: '**/[^_]*.md', base: "./src/data/page" }),
     schema: z.object({
-        
+        title: z.string(),
+        description: z.string().optional(),
+        pubDate: z.coerce.date().optional(),
     })
 });
 
-export const collections = { page };
+const deed = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/data/deed" }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+    })
+});
+
+export const collections = { page, deed };
---
+++ src/deed.config.ts
@@ -0,0 +1,31 @@
+export const SITE:{
+    TITLE: string;
+    DESCRIPTION: string;
+    LOGO: string;
+} ={
+    TITLE: "中文博客契约",
+    DESCRIPTION: "以诚实、理性与开放的精神，重建中文博客的尊严。",
+    LOGO: "/logo.svg",
+}
+
+export const NavLinks:{
+    HREF: string;
+    NAME: string;
+}[] = [
+    {
+        HREF: "/deeds",
+        NAME: "版本库"
+    },
+    {
+        HREF: "/about",
+        NAME: "关于"
+    },
+    {
+        HREF: "/privacy",
+        NAME: "隐私策略"
+    },
+    {
+        HREF: "/terms",
+        NAME: "服务条款"
+    },
+]
