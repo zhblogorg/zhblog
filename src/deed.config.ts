13404fa530c4d357a597ba1657025448eadb8af3
ymc0182 <501414437@qq.com>
2025-12-24 18:32:48 +0800

    update head

--- src/deed.config.ts
+++ src/deed.config.ts
@@ -5,7 +5,7 @@ export const SITE:{
     LOGO_W: string;
 } ={
     TITLE: "中文博客契约",
-    DESCRIPTION: "以诚实、理性与开放的精神，重建中文博客的尊严。",
+    DESCRIPTION: "中文博客契约是各位博主监督著作并严格遵守的非正式契约，我们共同维护中文独立博客生态环境，以诚实、理性与开放的精神，重建中文博客的尊严。",
     LOGO: "/logo.svg",
     LOGO_W: "/logo.w.svg",
 }
