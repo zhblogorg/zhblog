cb70430ce6487aa32391d8a451169d3a732ca73d
ymc0182 <501414437@qq.com>
2026-01-13 18:42:04 +0800

    添加网站地图

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,23 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  vite: {
+      plugins: [tailwindcss()],
+  },
 
-    server: {
-        host: true,
-        port: 4321,
-    },
+  server: {
+      host: true,
+      port: 4321,
+  },
 
-    devToolbar: {
-        enabled: false
-    },
+  devToolbar: {
+      enabled: false
+  },
 
-    site: "https://zhblog.org",
+  site: "https://zhblog.org",
+  integrations: [sitemap()],
 });
