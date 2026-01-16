8e58927a2a8342c6ce1bb587014cf36d0183f240
ymc0182 <501414437@qq.com>
2026-01-16 17:53:49 +0800

    update

--- src/deed.config.ts
+++ src/deed.config.ts
@@ -1,11 +1,13 @@
 export const SITE:{
     TITLE: string;
     DESCRIPTION: string;
+    KEYWORDS: string;
     LOGO: string;
     LOGO_W: string;
 } ={
     TITLE: "中文博客契约",
     DESCRIPTION: "中文博客契约是各位博主监督著作并严格遵守的非正式契约，我们共同维护中文独立博客生态环境，以诚实、理性与开放的精神，重建中文博客的尊严。",
+    KEYWORDS: "博客,独立博客,中文博客,博客契约,blog,blog covenant,zhblogcovenant,zhblog,covenant,deed",
     LOGO: "/logo.svg",
     LOGO_W: "/logo.w.svg",
 }
